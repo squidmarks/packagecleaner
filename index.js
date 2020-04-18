a542f1e03bbcb11c4d24d5f731b1d3accd4fb91e
Geoff Gerhardt <geoff.gerhardt@gmail.com>
2020-04-18 19:12:25 +0100

    GPIO working

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var gpio = require('onoff').Gpio
 
-const switch_1 = new Gpio(6, 'out')
-const switch_2 = new Gpio(13, 'out')
-const switch_3 = new Gpio(19, 'out')
-const switch_4 = new Gpio(26, 'out')
+const switch_1 = new gpio(6, 'out')
+const switch_2 = new gpio(13, 'out')
+const switch_3 = new gpio(19, 'out')
+const switch_4 = new gpio(26, 'out')
 
 setInterval(() => {
   const state = switch_1.readSync() === 0 ? 1:0
-  switch_1(state)
-  switch_2(state)
-  switch_3(state)
-  switch_4(state)
-},1000)
+  switch_1.writeSync(state)
+  switch_2.writeSync(state)
+  switch_3.writeSync(state)
+  switch_4.writeSync(state)
+},200)
 
