f57f2421c4465a0ea9aba65462b76dd0f42c42d4
Geoff Gerhardt <geoff.gerhardt@gmail.com>
2020-04-18 15:47:27 -0400

    ON/OFF defined

--- index.js
+++ index.js
@@ -4,41 +4,49 @@ const gpio = require('onoff').Gpio
 const TelegramService = require('./telegram')
 
 const defaultCleaningTime = 20
+const OFF = 1
+const ON = 0
 
 const telegram = new TelegramService(processCommand)
 
 function appCleanup() {
   telegram.shutdown()
+  redLight.unexport()
+  ozoneGenerator.unexport()
+  switch_4.unexport()
+  lidSwich.unexport()  
 };
 
 const ozoneGenerator = new gpio(6, 'out')
 const redLight = new gpio(13, 'out')
 const greenLight = new gpio(19, 'out')
 const switch_4 = new gpio(26, 'out')
+const lidSwich = new Gpio(4, 'in', 'rising', {debounceTimeout: 10})
 
-ozoneGenerator.writeSync(0)
-redLight.writeSync(0)
-greenLight.writeSync(0)
+ozoneGenerator.writeSync(OFF)
+redLight.writeSync(OFF)
+greenLight.writeSync(OFF)
+switch_4.writeSync(OFF)
 
 function startCleaningCycle(minutes, fromId) {
   console.log('Starting cleaning cycle')
   telegram.sendMessage(`Starting ${minutes || defaultCleaningTime} cleaning cycle...`, fromId)
-  ozoneGenerator.writeSync(1)
-  redLight.writeSync(1)
-  greenLight.writeSync(0)
+  ozoneGenerator.writeSync(ON)
+  redLight.writeSync(ON)
+  greenLight.writeSync(OFF)
   setTimeout(() => {
-    ozoneGenerator.writeSync(0)
-    redLight.writeSync(0)
-    greenLight.writeSync(1)  
+    ozoneGenerator.writeSync(OFF)
+    redLight.writeSync(OFF)
+    greenLight.writeSync(ON)  
     console.log('Cleaning cycle completed')
     telegram.sendMessage(`${minutes || defaultCleaningTime} cleaning cycle completed!`, fromId)
   }, (minutes || defaultCleaningTime) * 1000)
 }
 
 function abortCleaningCycle(fromId) {
-  ozoneGenerator.writeSync(0)
-  redLight.writeSync(0)
-  greenLight.writeSync(0)  
+  ozoneGenerator.writeSync(OFF)
+  redLight.writeSync(OFF)
+  greenLight.writeSync(OFF)  
   console.log('Cleaning cycle aborted')
   telegram.sendMessage(`Cleaning cycle aborted`, fromId)
 }
